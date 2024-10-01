8b7472fe1329f930e54017c2981f33e3bd54a09e
Devanshi Bhatevara <devanshi8730@gmail.com>
2024-10-01 18:20:12 +0530

    added MVC Project

---
+++ DemoCrudMVC/Models/DemoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoCrudMVC.Models
+{
+    public class DemoViewModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
+        public int Age { get; set; }
+    }
+}
